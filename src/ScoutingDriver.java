d43e09237ff399e50304cd79e4adbc6ab23b5482
PhilipCanete <philcanete1@gmail.com>
2015-10-30 22:53:11 -0700

    Removed unnecessary constructor
    Just testing out github, sorry!

--- src/ScoutingDriver.java
+++ src/ScoutingDriver.java
@@ -1,10 +1,7 @@
 
 public class ScoutingDriver {
 
-	public ScoutingDriver() {
-		// TODO Auto-generated constructor stub
-	}
-
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 
